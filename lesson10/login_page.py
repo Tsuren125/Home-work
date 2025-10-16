aeb2c7ec5d14b525b1c3f8776692f13aaf1513fd
Tsuren125 <gom6oev@gmail.com>
2025-10-16 12:01:01 +0900

    Update login_page.py

--- lesson10/login_page.py
+++ lesson10/login_page.py
@@ -17,4 +17,3 @@ class LoginPage:
         self.driver.find_element(*self.username_field).send_keys(username)
         self.driver.find_element(*self.password_field).send_keys(password)
         self.driver.find_element(*self.login_button).click()
-
